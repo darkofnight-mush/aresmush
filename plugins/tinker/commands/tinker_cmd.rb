6711dd4c3a05470d4be157c9eb1e5bc4a380c4da
Admin <admin@darkofnight.aresmush.com>
2022-04-23 19:57:46 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -3,18 +3,23 @@ module AresMUSH
     class TinkerCmd
       include CommandHandler
       
+      attr_accessor :asp, :term, :goal, :asp_list
+      
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
       
+      @asp_list = Array.new
+      
       def handle
-        piggies = integer_arg(cmd.args)
-        if (piggies < 5)
-          client.emit_ooc "#{piggies} is a small number of piggies."
-        else
-          client.emit_ooc "#{piggies} is a lot of piggies!"
-        end
+        args = cmd.parse_args(ArgParser.arg1_equals_arg2)
+        self.term = args.arg1
+        self.goal = args.arg2
+        self.asp = { goal => term }
+        @asp_list >> self.asp
+        client.emit_success "Done!"
+        
       end
 
     end
